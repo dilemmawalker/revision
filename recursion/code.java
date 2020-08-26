83dbe23990931c1feced6663fa47781acd1bcd8d
yash handa <yashhanda500@gmail.com>
2020-08-26 20:51:54 +0530

    update

--- recursion/code.java
+++ recursion/code.java
@@ -2,7 +2,21 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class code{
+    public static void increasing(int a,int b){
+        if(a==b)
+        return;
+
+        System.out.print(a+" ");
+        increasing(a+1,b);
+    }
+    public static void decreasing(int a,int b){
+        if(a==b)
+        return;
+
+        decreasing(a+1,b);
+        System.out.print(a+" ");
+    }
     public static void main(String[]args){
-       System.out.print("sdfghj");
+       decreasing(0,10);
     }
 }
