2ba2042f837ca1376d3421dec6224099eaf65b91
yash handa <yashhanda500@gmail.com>
2020-08-26 21:21:33 +0530

    update

--- recursion/code.java
+++ recursion/code.java
@@ -9,14 +9,33 @@ public class code{
         System.out.print(a+" ");
         increasing(a+1,b);
     }
-    public static void decreasing(int a,int b){
+    public static void decreasing(int a,int b){                        
         if(a==b)
         return;
 
         decreasing(a+1,b);
         System.out.print(a+" ");
     }
+    public static void evenodd(int a,int b){
+        if(a==b)
+        return;
+
+        if(a%2==0)
+        System.out.print(a+" ");
+        evenodd(a+1,b);
+        if(a%2!=0)
+        System.out.print(a+" ");
+        return;
+    }
+    public static int fact(int a,int b){
+        if(a==b)
+        return a;
+
+        int c=1;
+        c=fact(a+1,b);
+        return c*a;
+    }
     public static void main(String[]args){
-       decreasing(0,10);
+       System.out.print(fact(1,5));
     }
 }
