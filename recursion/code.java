9aa49b1a12b8382ac154eff824cbfdf3e2bd318d
yash handa <yashhanda500@gmail.com>
2020-08-27 02:03:01 +0530

    update

--- recursion/code.java
+++ recursion/code.java
@@ -36,11 +36,37 @@ public class code{
         c=fact(a+1,b);
         return c*a;
     }
+    public static void display(int vidx, int []arr){
+        if(vidx==arr.length)
+        return;
+
+        System.out.print(arr[vidx]+" ");
+        display(vidx+1,arr);
+    }
+    public static boolean find(int vidx, int []arr,int a){
+        if(vidx==arr.length)
+        return false;
+
+        if(a==arr[vidx])
+        return true;
+        boolean flag=false;
+       flag= find(vidx+1,arr,a);
+       return flag;
+    }
+    public static int max(int vidx,int []arr){
+        if(vidx==arr.length)
+        return 0;
+
+        int a=0;
+        return Math.max(arr[vidx],max(vidx+1,arr));
+    }
     
     public static void main(String[]args){
         int n=scn.nextInt();
-        int[]dp=new int[n+1];
+        // int[]dp=new int[n+1];
     //    System.out.print(fact(1,n));
-    calls(n,dp);
+    // calls(n,dp);
+    int[]arr={1,2,34,5,6,7,5,4,3,6,70};
+    System.out.println(max(0,arr));
     }
 }
