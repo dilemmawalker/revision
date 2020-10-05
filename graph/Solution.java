01f4c8968be862ee86639434a9a49e4b2c92b672
yash handa <yashhanda500@gmail.com>
2020-10-05 19:23:41 +0530

    up

--- graph/Solution.java
+++ graph/Solution.java
@@ -8,7 +8,8 @@ public class Solution{
         HashMap<Integer,Integer>map=new HashMap<>();
         ArrayList<Integer>ar=new ArrayList<>();
            int n=scn.nextInt();
-           while(n-->0){
+           int n1=n;
+           while(n1-->0){
                int a=scn.nextInt();
                 if(!map.containsKey(a)){
                     ar.add(a);
@@ -19,57 +20,63 @@ public class Solution{
                 }
            }
            int size=ar.size();
-           int[][]arr=new int[size][2];
-           for(int i=0;i<size;i++){
-               arr[i][0]=ar.get(i);
-               arr[i][1]=map.get(ar.get(i));
-           }
+        //    int[][]arr=new int[size][2];
+        //    for(int i=0;i<size;i++){
+        //        arr[i][0]=ar.get(i);
+        //        arr[i][1]=map.get(ar.get(i));
+        //    }
            if(ar.size()==1){
                System.out.println(0);
                continue;
            }
-           Arrays.sort(arr,(int[]a,int[]b)->{
+        //    Arrays.sort(arr,(int[]a,int[]b)->{
+        //        return b[1]-a[1];
+        //    });
+           //
+        //    for(int i=0;i<arr.length;i++)
+        //    System.out.print(arr[i][0]+" ");
+        //    System.out.println();
+           //
+           PriorityQueue<int[]>pq=new PriorityQueue<>((int[]a,int[]b)->{
                return b[1]-a[1];
            });
-           //
-           for(int i=0;i<arr.length;i++)
-           System.out.print(arr[i][0]+" ");
-           System.out.println();
-           //
-           StringBuilder ans=new StringBuilder("");
-           int ei=arr.length-1;
-           while(ei!=0){
-               for(int i=0;i<arr.length;i++){
-                int s=arr[i][0];
-                int val=arr[i][1];
-                if(val==0){
-                    ei=i;
-                    break;
-                }
-                arr[i][1]--;
-                ans.append(s);
-               }
-           }
-        //    while(arr[0][1]!=0){
-        //        int s=arr[i][0];
-        //        int val=arr[i][1];
-        //        if(i==0 && val==0)
-        //        break;
-        //        if(val==0)
-        //        continue;
-        //         ans.append(s);
-        //         arr[i][1]--;
-        //    }
+        for(int i=0;i<size;i++){
+            int val=ar.get(i);
+            pq.add(new int[]{val,map.get(val)});
+        }
            boolean flag=false;
-           char ch=ans.charAt(ans.length()-1);
-           int co=0;
-           for(int i=ans.length()-2;i>=0;i--){
-               char c=ans.charAt(i);
-               if(c==ch)
-               break;
-               co++;
+           while(!flag){
+               if(solve(size,pq,map,n)){
+                   System.out.println(size);
+                   flag=true;
+                   break;
+               }
+               size--;
            }
-           System.out.println(co);
+           System.out.println(size);
        }
+}
+public static boolean solve(int size,PriorityQueue<int[]>que1,HashMap<Integer,Integer>que2,int n){
+    int[]arr=new int[n];
+    for(int i=0;i<n;i++){
+        int a=arr[i];
+        if(a!=0){
+        que1.add(new int[]{a,que2.get(a)});
+        que2.remove(a);
+        }
+        if(que1.size()==0)
+        return false;
+        
+        int[]ar=que1.remove();
+        if(ar[1]!=0){
+        que2.put(ar[0],ar[1]-1);
+        que1.remove(ar[0]);
+        if(i+size<n)
+        arr[i+size]=ar[0];
+        }
+        else return false;
+
+    }
+    return true;
 }
 }
